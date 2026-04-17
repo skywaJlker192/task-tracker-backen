e6f29ce639b61185d920fe1346f7d2f1a76002cc
student <student@ex.ru>
2026-04-17 14:41:34 +0300

    улучшил decode_access_token + добавил правильный HTTPException

--- app/core/security.py
+++ app/core/security.py
@@ -2,7 +2,7 @@ from passlib.context import CryptContext
 from jose import JWTError, jwt
 from datetime import datetime, timedelta, timezone
 from app.core.config import settings
-
+from fastapi import HTTPException, status
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_password(password: str) -> str:
@@ -22,4 +22,7 @@ def decode_access_token(token: str) -> dict:
         payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
         return payload
     except JWTError:
-        raise Exception("Некорректный или просроченный токен")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Некорректный или просроченный токен"
+        )
