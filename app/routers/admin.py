753631eeabaa37df2c6568c5a2b6fda1b406ff0c
student <student@ex.ru>
2026-04-17 14:25:50 +0300

    добавил строку router = APIRouter() в admin.py

--- app/routers/admin.py
+++ app/routers/admin.py
@@ -4,7 +4,7 @@ from app.core.database import get_db
 from app.dependencies.auth import get_current_admin
 from sqlalchemy.orm import Session
 
-router = APIRouter()
+
 
 @router.get("/users")
 def list_users(db: Session = Depends(get_db), admin = Depends(get_current_admin)):
