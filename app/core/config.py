6ab794b4e9d4158209050e7b58a85b1a56bde143
student <student@ex.ru>
2026-04-17 14:22:59 +0300

    изменил срок действия доступа токена

--- app/core/config.py
+++ app/core/config.py
@@ -1,11 +1,11 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettingss
 # Конфигурация проекта
 # Здесь хранятся все настройки: база данных, секретный ключ, Redis и т.д.
 class Settings(BaseSettings):
     DATABASE_URL: str = "sqlite:///./app.db"
     SECRET_KEY: str = "super_secret_key_for_demo_project_change_me"
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 50
     REDIS_URL: str = "redis://localhost:6379/0"
 
 settings = Settings()
